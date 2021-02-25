74fc56dbca15f3317c402495ce1d5d08a1c0261a
Neiro Somemiya <nirot1r@g-second.net>
2021-02-25 16:54:01 +0900

    スニークで設置

--- src/main/java/jp/nrsy/mc/nervous/listeners/events/AutoMinecartSpawner.java
+++ src/main/java/jp/nrsy/mc/nervous/listeners/events/AutoMinecartSpawner.java
@@ -4,12 +4,15 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
 import org.bukkit.inventory.EquipmentSlot;
+
+import jp.nrsy.mc.nervous.helpers.MessageHelper;
 
 public class AutoMinecartSpawner implements Listener {
 
@@ -21,11 +24,17 @@ public class AutoMinecartSpawner implements Listener {
 
     Location loc = event.getClickedBlock().getLocation().add(0, 1, 0);
     event.getClickedBlock().getWorld().spawn(loc, Minecart.class);
+
+    MessageHelper.success(event.getPlayer(), "トロッコを設置しました。");
   }
 
   @EventHandler
   public void onLeaveEvent(VehicleExitEvent event) {
+    if(event.getExited().getType() != EntityType.PLAYER) return;
     if(event.getVehicle().getType() != EntityType.MINECART) return;
     event.getVehicle().remove();
+
+    Player player = (Player) event.getExited();
+    MessageHelper.success(player, "トロッコを撤去しました。");
   }
 }
